f88336cbad46a6ef1f890eebc6a8cd53145e8c35
ehemhuy <ehemhuy@gmail.com>
2021-09-05 21:07:47 +0700

    a

--- go.mod
+++ go.mod
@@ -3,22 +3,35 @@ module ehemhuy/golang-gingonic
 go 1.17
 
 require (
+	github.com/PuerkitoBio/goquery v1.7.1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.3.6 // indirect
+	github.com/antchfx/xpath v1.2.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.7.4 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly/v2 v2.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gocolly/colly/v2"
 	"github.com/joho/godotenv"
 )
+
+type ReqBody struct {
+	Url  string `json:"url"`
+	Page string
+}
+
+type ReqQuery struct {
+	Url  string `json:"url"`
+	Page int    `json:"page" form:"page"`
+	Size int    `json:"size" form:"size"`
+}
+
+type Zone struct {
+	Url   string `json:"url"`
+	Title string `json:"title"`
+}
+
+type Image struct {
+	Uri string `json:"uri"`
+}
 
 func main() {
 	var router = gin.Default()
@@ -17,6 +37,72 @@ func main() {
 		c.JSON(http.StatusOK, "ok")
 
 	})
+
+	//get khu vuc
+	router.GET("/zone/list", func(c *gin.Context) {
+		var zones []interface{}
+
+		collector := colly.NewCollector()
+
+		collector.OnHTML(".node-title a", func(element *colly.HTMLElement) {
+			var zone Zone
+			zone.Url = "https://checkerviet.me" + element.Attr("href")
+			zone.Title = element.Text
+			zones = append(zones, zone)
+		})
+
+		collector.OnScraped(func(r *colly.Response) {
+
+			c.JSON(http.StatusOK, zones)
+		})
+
+		collector.Visit("https://checkerviet.me/forums/gai-goi-ha-noi.6/")
+	})
+
+	// get chi tiet 1 khu vuc
+	router.POST("/zone/detail", func(ctx *gin.Context) {
+		var zone Zone
+		ctx.ShouldBindJSON(&zone)
+		var zones []Zone
+
+		c := colly.NewCollector()
+
+		// Find and visit all links
+		c.OnHTML(".block-body .structItem-title a:last-child", func(e *colly.HTMLElement) {
+			zone := Zone{
+				Url:   "https://checkerviet.me" + e.Attr("href"),
+				Title: e.Text,
+			}
+			zones = append(zones, zone)
+		})
+
+		c.OnScraped(func(r *colly.Response) {
+			ctx.JSON(http.StatusOK, zones)
+		})
+
+		c.Visit(zone.Url)
+	})
+
+	// get anh tu link
+	router.POST("/images", func(ctx *gin.Context) {
+		var body map[string]string
+		var images []Image
+		ctx.ShouldBindJSON(&body)
+		c := colly.NewCollector()
+
+		c.OnHTML("img.bbImage", func(h *colly.HTMLElement) {
+			var image = Image{
+				Uri: h.Attr("data-url"),
+			}
+			images = append(images, image)
+		})
+
+		c.OnScraped(func(r *colly.Response) {
+			ctx.JSON(http.StatusOK, images)
+		})
+
+		c.Visit(body["url"])
+	})
+
 	router.Run(":" + port)
-	fmt.Printf("ok")
 }
